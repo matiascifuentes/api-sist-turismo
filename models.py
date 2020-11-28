aa5bdd902706f2203161fd1960f839437054be0d
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2020-11-27 23:49:10 -0300

    Agrego clase Restaurant a models.

--- models.py
+++ models.py
@@ -27,3 +27,27 @@ class Hotel(db.Model):
             'url_mapa': self.url_mapa
         }
 
+class Restaurant(db.Model):
+    __tablename__ = 'restaurant'
+
+    nombre = db.Column(db.String(200),nullable=False)
+    url = db.Column(db.String(200),primary_key=True)
+    ciudad = db.Column(db.String(50))
+    direccion = db.Column(db.String(200))
+    num_valoracion = db.Column(db.String(50))
+    valoracion = db.Column(db.String(50))
+    telefono = db.Column(db.String(20))
+    url_mapa = db.Column(db.String(500))
+
+    def json(self):
+        return {
+            'nombre': self.nombre,
+            'url': self.url,
+            'ciudad': self.ciudad,
+            'direccion': self.direccion,
+            'num_valoracion': self.num_valoracion,
+            'valoracion': self.valoracion,
+            'telefono': self.telefono,
+            'url_mapa': self.url_mapa
+        }
+        
