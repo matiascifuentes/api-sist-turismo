b7edf7bdac80b8347e08f23638806f52cfe022ca
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2020-11-27 23:46:27 -0300

    Agrego archivo models con clase Hotel.

---
+++ models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Hotel(db.Model):
+    __tablename__ = 'hotel'
+
+    nombre = db.Column(db.String(200),nullable=False)
+    url = db.Column(db.String(200),primary_key=True)
+    descripcion = db.Column(db.String(500))
+    ciudad = db.Column(db.String(50))
+    direccion = db.Column(db.String(200))
+    num_valoracion = db.Column(db.String(50))
+    valoracion = db.Column(db.String(50))
+    url_mapa = db.Column(db.String(500))
+
+    def json(self):
+        return {
+            'nombre': self.nombre,
+            'url': self.url,
+            'descripcion': self.descripcion,
+            'ciudad': self.ciudad,
+            'direccion': self.direccion,
+            'num_valoracion': self.num_valoracion,
+            'valoracion': self.valoracion,
+            'url_mapa': self.url_mapa
+        }
+
