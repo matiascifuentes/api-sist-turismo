f3be26deb4150468cdac7a94d3b156300851b3fb
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2020-11-29 13:32:53 -0300

    Agrego función get_restaurant(cod_restaurant) a la api.

--- main.py
+++ main.py
@@ -27,13 +27,19 @@ def get_hotel(cod_hotel):
     hotel = Hotel.query.filter_by(cod_hotel=cod_hotel).first()
     if hotel is None:
         return jsonify({'message': 'El hotel no existe'}), 404
-
     return jsonify({'hotel': hotel.json() })
 
 @app.route('/api/v1/restaurants', methods=['GET'])
 def get_restaurants():
     restaurants = [restaurant.json() for restaurant in Restaurant.query.all()]
     return jsonify({'restaurants': restaurants })
+
+@app.route('/api/v1/restaurants/<cod_restaurant>', methods=['GET'])
+def get_restaurant(cod_restaurant):
+    restaurant = Restaurant.query.filter_by(cod_restaurant=cod_restaurant).first()
+    if restaurant is None:
+        return jsonify({'message': 'El restaurant no existe'}), 404
+    return jsonify({'restaurant': restaurant.json() })
 
 if __name__ == '__main__':
     app.run(debug=True)
