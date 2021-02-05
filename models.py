891154aa1feae9d259ae5887c35e45ca3d42ac85
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-05 14:01:24 -0300

    Modifico clase Restaurant.

--- models.py
+++ models.py
@@ -43,26 +43,10 @@ class Hotel(db.Model):
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
 
-    cod_restaurant = db.Column(db.String(20),primary_key=True)
-    nombre = db.Column(db.String(200),nullable=False)
-    url = db.Column(db.String(200),nullable=False)
-    ciudad = db.Column(db.String(50))
-    direccion = db.Column(db.String(200))
-    num_valoracion = db.Column(db.String(50))
-    valoracion = db.Column(db.String(50))
-    telefono = db.Column(db.String(20))
-    url_mapa = db.Column(db.String(500))
+    id_servicio = db.Column(db.String(20),primary_key=True,ForeignKey(servicio.id_servicio))
 
     def json(self):
         return {
-            'cod_restaurant': self.cod_restaurant,
-            'nombre': self.nombre,
-            'url': self.url,
-            'ciudad': self.ciudad,
-            'direccion': self.direccion,
-            'num_valoracion': self.num_valoracion,
-            'valoracion': self.valoracion,
-            'telefono': self.telefono,
-            'url_mapa': self.url_mapa
+            'id_servicio': self.id_servicio
         }
         
