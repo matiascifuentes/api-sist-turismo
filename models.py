803eff2f1f0cf137438c1cdf53191725b2e48635
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-05 13:54:32 -0300

    Agrego clase Servicio.

--- models.py
+++ models.py
@@ -3,6 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Servicio(db.Model):
+    __tablename__ = 'servicio'
+
+    id_servicio = db.Column(db.String(20),primary_key=True)
+    nombre = db.Column(db.String(200),nullable=False)
+    url = db.Column(db.String(200),nullable=False)
+    direccion = db.Column(db.String(200))
+    ciudad = db.Column(db.String(50))
+    descripcion = db.Column(db.String(500))
+    valoracion = db.Column(db.String(50))
+    num_valoracion = db.Column(db.String(50))
+    url_mapa = db.Column(db.String(500))
+
+    def json(self):
+        return {
+            'id_servicio': self.id_servicio,
+            'nombre': self.nombre,
+            'url': self.url,
+            'direccion': self.direccion,
+            'ciudad': self.ciudad,
+            'descripcion': self.descripcion,
+            'valoracion': self.valoracion,
+            'num_valoracion': self.num_valoracion,
+            'url_mapa': self.url_mapa
+        }
+
+
 class Hotel(db.Model):
     __tablename__ = 'hotel'
 
