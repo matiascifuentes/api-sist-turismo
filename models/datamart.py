a86972f541214c5e7ac300587eaec2e9e8af9ce1
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-21 17:29:51 -0300

    Agrego clase HistoricoListas

--- models/datamart.py
+++ models/datamart.py
@@ -1,10 +1,20 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+dm = SQLAlchemy()
 
 class HistoricoListas(db.Model):
-    pass
+    __tablename__ = 'historico_listas'
+
+    id_lista = db.Column(db.Integer,primary_key=True)
+    id_servicio = db.Column(db.String(20)primary_key=True)
+
+    def json(self):
+        return {
+            'id_lista': self.id_lista,
+            'id_servicio': self.id_servicio
+        }
+
 
 class HistoricoSesiones(db.Model):
     pass
