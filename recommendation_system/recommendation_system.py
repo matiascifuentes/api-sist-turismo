5088305498b28c11e3ae8a022e0e4b1a5a764364
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-21 22:28:15 -0300

    Agrego función recommendations.

--- recommendation_system/recommendation_system.py
+++ recommendation_system/recommendation_system.py
@@ -3,3 +3,12 @@ from efficient_apriori import apriori
 def generate_association_rules(transactions, min_support, min_confidence):
 	itemsets, rules = apriori(transactions, min_support=min_support, min_confidence=min_confidence)
 	return rules
+
+def recommendations(service, rules, max_results):
+	result = []
+	for rule in rules:
+		if len(result) == max_results:
+			break
+		if rule.lhs[0] == service:
+			result.append(rule.rhs[0])
+	return result
