77d9ab0b618106eeb4737b63c6c13b3776ad5092
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-22 19:15:46 -0300

    Agrego save_rules_to_file

--- recommendation_system/recommendation_system.py
+++ recommendation_system/recommendation_system.py
@@ -1,4 +1,6 @@
 from efficient_apriori import apriori
+from rule import Rule
+import json
 
 def generate_association_rules(transactions, min_support, min_confidence):
 	itemsets, rules = apriori(transactions, min_support=min_support, min_confidence=min_confidence)
@@ -12,3 +14,15 @@ def recommendations(service, rules, max_results):
 		if rule.lhs[0] == service:
 			result.append(rule.rhs[0])
 	return result
+
+def save_rules_to_file(rules):
+	data = {}
+	data['rules'] = []
+	for rule in rules:
+		data['rules'].append(Rule(rule.lhs[0],rule.rhs[0],rule.support,rule.confidence,rule.lift).json())
+
+	with open('rules.json', 'w') as file:
+		json.dump(data, file)
+
+
+
