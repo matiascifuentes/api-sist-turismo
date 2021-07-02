69067e7efe946c8c0d7ac511366d4955a0b1066d
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-07-02 01:52:46 -0400

    soluciono filtro reglas

--- recommendation_system/recommendation_system.py
+++ recommendation_system/recommendation_system.py
@@ -1,6 +1,6 @@
 from efficient_apriori import apriori
 from recommendation_system.rule import Rule
-from recommendation_system.utils import get_itemset_historico_sesiones
+from recommendation_system.utils import get_itemset_historico_sesiones, get_itemset_historico_listas
 import json
 
 def generate_association_rules(transactions, min_support, min_confidence):
@@ -18,15 +18,15 @@ def recommendations(service, rules, max_results):
 
 def save_rules_to_file(rules):
 	min_lift = 1
-	rules = filter(lambda rule: rule.lift >= min_lift, rules)
-	rules = sorted(rules, key=lambda rule: rule.lift)
+	rules = sorted(rules, key=lambda rule: rule.lift, reverse=True)
+	rules = filter(lambda rule: rule.lift > min_lift, rules)
 	data = {}
 	data['rules'] = []
 	for rule in rules:
 		data['rules'].append(Rule(rule.lhs[0],rule.rhs[0],rule.support,rule.confidence,rule.lift).json())
 
-	with open('rules_sesiones.json', 'w') as file:
-		json.dump(data, file)
+	with open('rules.json', 'w') as file:
+		json.dump(data, file)	#json.dump(data, file, indent=4)
 
 def get_rules_from_file():
 	rules = []
@@ -44,11 +44,20 @@ def get_rules_from_sessions(user_id):
 	try:
 		min_lift = 1
 		itemset = get_itemset_historico_sesiones(user_id,1000)
-		rules_aux = generate_association_rules(itemset,0.1,0.1)
-		rules_aux = filter(lambda rule: rule.lift >= min_lift, rules_aux)
-		rules_aux = sorted(rules_aux, key=lambda rule: rule.lift)
+		rules_aux = generate_association_rules(itemset,0.1,0.3)
+		rules_aux = sorted(rules_aux, key=lambda rule: rule.lift, reverse=True)
+		rules_aux = filter(lambda rule: rule.lift > min_lift, rules_aux)
 		for rule in rules_aux:
 			rules.append(Rule(rule.lhs[0],rule.rhs[0],rule.support,rule.confidence,rule.lift))
 		return True, rules
 	except:
 		return False, rules
+
+def save_rules():
+	try:
+		itemset = get_itemset_historico_listas()
+		rules = generate_association_rules(itemset,0.1,0.1)
+		save_rules_to_file(rules)
+		return True
+	except:
+		return False
